93c3eb8bab904fc67482a5e6920d772210737cb1
Philip Loa <boyet007@gmail.com>
2019-09-04 13:23:06 +0700

    step 1

---
+++ src/AllForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AllForm extends Component {
+    render() {
+        return (
+            <div>
+            <h1>All Posts</h1>
+            </div>
+        )
+    }
+}
+
+export default AllForm
